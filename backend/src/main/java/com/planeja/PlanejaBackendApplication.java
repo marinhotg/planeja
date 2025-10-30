8925954cef8723d86e8557bd8abab425db4ddafe
marinhotg <tiago.limarinho@gmail.com>
2025-10-30 00:40:11 -0300

    feat: add ping endpoint to prevent render hibernation

--- backend/src/main/java/com/planeja/PlanejaBackendApplication.java
+++ backend/src/main/java/com/planeja/PlanejaBackendApplication.java
@@ -2,8 +2,10 @@ package com.planeja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PlanejaBackendApplication {
 
 	public static void main(String[] args) {
